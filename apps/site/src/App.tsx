3981ca653adbb7a7e18da8161db5f650e16a83ff
Samuel Elie <selie@anduril.com>
2022-05-25 16:37:44 -0700

    build

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
+import { createRef, useEffect, useRef, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 import Worker from './worker/flux.worker?worker'
+import { fromEvent, merge, Subject } from 'rxjs'
+import { takeUntil, switchMap, tap, bufferTime } from 'rxjs/operators'
+
+type Command = string
 
 function arrayToStream() {
   const { readable, writable } = new TransformStream()
@@ -13,8 +17,25 @@ function arrayToStream() {
 
 function printToElementStream(message = '') {
   return new WritableStream({
-    write(chunk) {
+    write(chunk: string) {
       console.log(message + chunk)
+    },
+    close() {
+      console.log('closed')
+    },
+  })
+}
+function forwardWorkerPayload(output$: Subject<any>) {
+  return new WritableStream({
+    write(chunk: string) {
+      const d = JSON.parse(chunk)
+      const status = `payload is  ~${chunk.length * 1e-6}Mb. Took ${
+        performance.now() - d.timestamp
+      }ms to receive & parse`
+      output$.next({
+        d,
+        status,
+      })
     },
     close() {
       console.log('closed')
@@ -24,12 +45,11 @@ function printToElementStream(message = '') {
 
 export const setupRxWorkerFlux = () => {
   const worker = new Worker()
+  const output$ = new Subject<any>()
 
   worker.addEventListener('message', (event) => {
     const [fromWorker] = event.data
-    fromWorker.pipeTo(
-      printToElementStream('Originated from worker: '),
-    )
+    fromWorker.pipeTo(forwardWorkerPayload(output$))
   })
 
   const { readable, writer } = arrayToStream()
@@ -41,14 +61,19 @@ export const setupRxWorkerFlux = () => {
   worker.postMessage([readable, writable], [readable, writable])
 
   return {
-    write(val: number) {
+    write(val: Command) {
       writer.write(val)
     },
+    output$,
   }
 }
 
 function App() {
-  const [writeToWorker, setSub1] = useState(0)
+  const [writeToWorker, setSub1] = useState('play')
+  const [status, setStatus] = useState('')
+  const [intensity, setIntense] = useState(1)
+  const ref = createRef<HTMLImageElement>()
+  const textRef = createRef<HTMLTextAreaElement>()
   const rxWorkerFlux = useRef<ReturnType<typeof setupRxWorkerFlux>>()
   useEffect(() => {
     rxWorkerFlux.current = setupRxWorkerFlux()
@@ -56,16 +81,75 @@ function App() {
 
   useEffect(() => {
     rxWorkerFlux.current?.write(writeToWorker)
+    let c = 0
+    const sub = rxWorkerFlux.current?.output$
+      .pipe(bufferTime(1000))
+      .subscribe((d) => {
+        const status = d.reduce((acc, v) => acc + '\n' + v.status, '')
+        c += d.length
+        if (c >= 10_000) {
+          setStatus(status)
+          c = 0
+        } else {
+          setStatus((s) => s + '\n' + status)
+        }
+        c++
+      })
+    return () => sub?.unsubscribe()
   }, [writeToWorker])
+
+  useEffect(() => {
+    rxWorkerFlux.current?.write(`intensity_${intensity}`)
+  }, [intensity])
+
+  useEffect(() => {
+    if (!ref.current) return
+
+    const el = ref.current
+    fromEvent(el, 'mousedown')
+      .pipe(
+        switchMap(() =>
+          fromEvent<MouseEvent>(el, 'mousemove').pipe(
+            tap((e) => {
+              el.style.left = e.clientX - 150 + 'px'
+              el.style.top = e.clientY - 150 + 'px'
+            }),
+            takeUntil(
+              merge(
+                fromEvent(el, 'mouseup'),
+                fromEvent(el, 'mouseleave'),
+              ),
+            ),
+          ),
+        ),
+      )
+      .subscribe()
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <button onClick={() => setSub1((s) => Math.random())}>
-          write command to worker
+        <div ref={ref} className="drag">
+          drag me
+        </div>
+        <button onClick={() => setSub1((s) => 'pause')}>pause</button>
+        <button onClick={() => setSub1((s) => 'play')}>play</button>
+        <div>intensity: {intensity}</div>
+        <button onClick={() => setIntense((s) => Math.min(s + 1, 7))}>
+          + data
         </button>
+        <button onClick={() => setIntense((s) => Math.max(s - 1, 1))}>
+          - data
+        </button>
+        <textarea
+          ref={textRef}
+          id="story"
+          name="story"
+          rows={50}
+          cols={120}
+          defaultValue="It was a dark and stormy night..."
+          value={status}
+        ></textarea>
       </header>
     </div>
   )
--- apps/site/src/worker/flux.worker.ts
+++ apps/site/src/worker/flux.worker.ts
@@ -1,30 +1,182 @@
-import { toWritableStream } from '@rxjs-ninja/rxjs-utility'
-import { interval } from 'rxjs'
-import { map } from 'rxjs/operators'
+import {
+  fromReadableStream,
+  toWritableStream,
+} from '@rxjs-ninja/rxjs-utility'
+import {
+  BehaviorSubject,
+  interval,
+  merge,
+  Subject,
+  timer,
+} from 'rxjs'
+import {
+  filter,
+  map,
+  switchMap,
+  takeUntil,
+  repeat,
+  tap,
+  distinctUntilChanged,
+} from 'rxjs/operators'
+import { generateJson } from 'json-generator'
 
 const ctx: Worker = self as any
 
-function doSomeWork(value) {
-  let sum = value
-  for (let i = 0; i < 1e6; i++) {
-    sum += Math.random()
-    sum -= Math.random()
-  }
-  return sum
+function doSomeWork() {
+  const payload = generateJson({
+    id: 'id;objectId',
+    children: [
+      50,
+      {
+        name: 'fullName',
+        age: 'int;0;10',
+      },
+    ],
+    currentJob: {
+      title: 'Developer',
+      salary: 'mask;',
+    },
+    jobs: [
+      20,
+      {
+        title: 'random;["developer", "medic", "teacher", "CEO"]',
+        salary: 'money',
+        people: [
+          50,
+          {
+            name: 'fullName',
+            age: 'int;0;10',
+            personalities: [
+              50,
+              {
+                title:
+                  'random;["introvert", "leader", "scientist", "cowgirl"]',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    maxRunDistance: 'float;1;20;1',
+    cpf: 'cpf',
+    cnpj: 'cnpj',
+    pretendSalary: 'money',
+    age: 'int;20;80',
+    gender: 'gender',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    phone: 'maskInt;+55 (83) 9####-####',
+    address: 'address',
+    hairColor: 'color',
+  })
+
+  return JSON.stringify({ timestamp: performance.now(), payload })
+}
+
+const intensityMap = {
+  1: {
+    children: 1,
+    jobs: 2,
+    people: 1,
+  },
+  2: {
+    children: 4,
+    jobs: 6,
+    people: 2,
+  },
+  3: {
+    children: 8,
+    jobs: 24,
+    people: 4,
+  },
+  4: {
+    children: 16,
+    jobs: 48,
+    people: 6,
+  },
+  5: {
+    children: 32,
+    jobs: 96,
+    people: 12,
+  },
+  6: {
+    children: 64,
+    jobs: 192,
+    people: 24,
+  },
+  7: {
+    children: 128,
+    jobs: 384,
+    people: 48,
+  },
+}
+type Intensities = keyof typeof intensityMap
+function doSomeSmallWork(i: Intensities) {
+  const payload = generateJson({
+    id: 'id;objectId',
+    children: [
+      intensityMap[i].children,
+      {
+        name: 'fullName',
+        age: 'int;0;10',
+      },
+    ],
+    currentJob: {
+      title: 'Developer',
+      salary: 'mask;',
+    },
+    jobs: [
+      intensityMap[i].jobs,
+      {
+        title: 'random;["developer", "medic", "teacher", "CEO"]',
+        salary: 'money',
+        people: [
+          intensityMap[i].people,
+          {
+            name: 'fullName',
+            age: 'int;0;10',
+            personalities: [
+              3,
+              {
+                title:
+                  'random;["introvert", "leader", "scientist", "cowgirl"]',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    maxRunDistance: 'float;1;20;1',
+    cpf: 'cpf',
+    cnpj: 'cnpj',
+    pretendSalary: 'money',
+    age: 'int;20;80',
+    gender: 'gender',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    phone: 'maskInt;+55 (83) 9####-####',
+    address: 'address',
+    hairColor: 'color',
+  })
+
+  return JSON.stringify({
+    timestamp: performance.now(),
+    payload,
+  })
 }
 
 function processFromMainThread() {
   return new TransformStream({
-    start() {
-      console.time('process')
-    },
+    start() {},
     transform(chunk, controller) {
-      controller.enqueue(doSomeWork(chunk))
+      controller.enqueue(doSomeWork())
     },
-    flush() {
-      console.timeEnd('process')
-    },
+    flush() {},
   })
+}
+
+interface State {
+  intensity: Intensities
 }
 
 ctx.onmessage = (event) => {
@@ -33,14 +185,46 @@ ctx.onmessage = (event) => {
   const toMainThread = new TransformStream()
 
   // transform input and write to output
-  readable.pipeThrough(processFromMainThread()).pipeTo(writable)
+  // readable.pipeThrough(processFromMainThread()).pipeTo(writable)
 
-  interval(1000)
-    .pipe(
-      map((val) => (val += 1)),
-      toWritableStream(toMainThread.writable),
-    )
-    .subscribe()
+  const sendFluxData$ = new Subject<string>()
+  const sendStatus$ = new Subject<string>()
+  const controlInput$ = new BehaviorSubject(true)
+  const controlOutpout$ = controlInput$.pipe(distinctUntilChanged())
+
+  const pause$ = controlOutpout$.pipe(filter((c) => !c))
+  const play$ = controlOutpout$.pipe(filter((c) => !!c))
+
+  const state: State = {
+    intensity: 1,
+  }
+
+  merge(
+    // respond to commands
+    fromReadableStream<string>(readable).pipe(
+      tap((c) => {
+        if (c === 'play' || c === 'pause') {
+          controlInput$.next(c === 'play')
+        } else {
+          const [intesity] = c.split('_')[1]
+          state.intensity = Number(intesity) as Intensities
+          console.log(state.intensity)
+        }
+        sendStatus$.next(`ackknowledged: ${c}`)
+      }),
+    ),
+    play$.pipe(
+      switchMap(() =>
+        timer(0, 16).pipe(
+          map((val) => doSomeSmallWork(state.intensity)),
+          tap((chunk) => sendFluxData$.next(chunk)),
+          takeUntil(pause$),
+        ),
+      ),
+    ),
+    sendFluxData$.pipe(toWritableStream(toMainThread.writable)),
+    // sendStatus$.pipe(toWritableStream(writable)),
+  ).subscribe()
 
   //@ts-expect-error Transferable doesnnt include streams
   self.postMessage([toMainThread.readable], [toMainThread.readable])
