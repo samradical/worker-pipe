91d80fad30e8ba8ef1573fa4ec6e0dc02bb89512
Samuel Elie <selie@anduril.com>
2022-06-01 12:22:34 -0700

    build

--- assets/follower.d1d6f19f.js
+++
@@ -1,1 +0,0 @@
-(function(){"use strict";ctx.onconnect=function(n){const e=new MessageChannel;var o=n.ports[0];console.log("follower connected"),o.onmessage=function(t){const{port:s,id:c}=t.data;e.port2.onmessage=function(a){const r=new TextDecoder().decode(a.data);o.postMessage([`leader told me: ${r}`])},s.postMessage({id:c,port:e.port1},[e.port1])}}})();
